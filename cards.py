7e864bfec9c06d3068a211d9c4b421a9e2056046
vokator-online <vokator@gmail.com>
2024-01-15 10:30:53 +0200

    Kortų užduotis startas

---
+++ cards.py
@@ -0,0 +1,13 @@
+# Kortų kaladė
+# Korta: objektas
+# -- rank (2-9, T, J, Q, K, A)
+# -- suit (spades, clubs, hearts, diamonds)
+# -- sign (suit + rank)
+# -- weight
+# Kortų kaladė
+# -- cards - sąrašas kortų
+# -- shuffle
+# -- take from top
+# -- take from bottom
+# -- take random
+# mastom apie žaidimą
